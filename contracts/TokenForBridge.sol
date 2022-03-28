55a58adf47254f33a3e8518dcee6c047bcad4e4e
Nyaksik <makapori@gmail.com>
2022-03-28 17:18:16 +0700

    fix role for TokenForBridge. bridge done

--- contracts/TokenForBridge.sol
+++ contracts/TokenForBridge.sol
@@ -2,19 +2,27 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 string constant NAME = "TokenForBridge";
 string constant SYMBOL = "TFB";
 
-contract TokenForBridge is ERC20, Ownable {
-    constructor() ERC20(NAME, SYMBOL) {}
+contract TokenForBridge is ERC20 {
+    address private bridge;
 
-    function mint(address account, uint256 amount) external onlyOwner {
-        _mint(account, amount);
+    constructor(address _bridge) ERC20(NAME, SYMBOL) {
+        bridge = _bridge;
     }
 
-    function burn(address account, uint256 amount) external onlyOwner {
-        _burn(account, amount);
+    modifier onlyBridge() {
+        require(bridge == msg.sender, "Only a bridge can do that.");
+        _;
+    }
+
+    function mint(address _account, uint256 _amount) external onlyBridge {
+        _mint(_account, _amount);
+    }
+
+    function burn(address _account, uint256 _amount) external onlyBridge {
+        _burn(_account, _amount);
     }
 }
