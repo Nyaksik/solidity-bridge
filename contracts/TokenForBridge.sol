3febfd40ccc75f82e8cec4f64a52e6a1df6d9389
Nyaksik <makapori@gmail.com>
2022-03-28 14:59:56 +0700

    init project. token erc20 for bridge

---
+++ contracts/TokenForBridge.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+string constant NAME = "TokenForBridge";
+string constant SYMBOL = "TFB";
+
+contract TokenForBridge is ERC20, Ownable {
+    constructor() ERC20(NAME, SYMBOL) {}
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external onlyOwner {
+        _burn(account, amount);
+    }
+}
